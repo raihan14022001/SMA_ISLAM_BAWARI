dc3c85cfb3dcc554726c88e8dc87f6a1915bbd11
Muhammad Raihan <raihan_raihan@student.untan.ac.id>
2023-09-15 20:27:52 +0700

    repair prestasi

--- resources/views/home_page/home/prestasi/index.blade.php
+++ resources/views/home_page/home/prestasi/index.blade.php
@@ -1,59 +1,59 @@
 @extends('home_page.app2')
 @section('head')
-    <title>Prestasi<title>
-    @endsection
+    <title>prestasi</title>
+@endsection
 
-    @section('isi')
-        <br>
+@section('isi')
+    <br>
 
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col">
-                    <div class="row">
-                        @if ($data != null)
-                            @foreach ($data as $dataBlog)
-                                <div class="col-sm-4 mb-6 mb-sm-0">
-                                    <div class="card">
-                                        <img src="{{ asset($dataBlog->image_thumbnail) }}" class="card-img-top" alt="gambar"
-                                            height="300">
-                                        {{-- <img src="{{ asset($data->image_thumbnail) }}" width='80' height='80'> --}}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    @if ($data != null)
+                        @foreach ($data as $dataBlog)
+                            <div class="col-sm-4 mb-6 mb-sm-0">
+                                <div class="card">
+                                    <img src="{{ asset($dataBlog->image_thumbnail) }}" class="card-img-top" alt="gambar"
+                                        height="300">
+                                    {{-- <img src="{{ asset($data->image_thumbnail) }}" width='80' height='80'> --}}
 
-                                        <div class="card-body">
-                                            <h5 class="card-title">{{ $dataBlog->judul }} || {{ $kategori }} </h5>
-                                            </p>
-                                            <a href="{{ route('blog.show', ['id' => $dataBlog->id]) }}"
-                                                class="btn btn-primary">Lihat</a>
-                                            {{-- <a href="{{ route('blog.show', ['id' => $val->id]) }}" class="btn btn-secondary"><i class="fa fa-info"></i></a> --}}
-                                        </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $dataBlog->judul }} || {{ $kategori }} </h5>
+                                        </p>
+                                        <a href="{{ route('blog.show', ['id' => $dataBlog->id]) }}"
+                                            class="btn btn-primary">Lihat</a>
+                                        {{-- <a href="{{ route('blog.show', ['id' => $val->id]) }}" class="btn btn-secondary"><i class="fa fa-info"></i></a> --}}
                                     </div>
                                 </div>
-                            @endforeach
-                        @else
-                            <p>no data</p>
-                        @endif
+                            </div>
+                        @endforeach
+                    @else
+                        <p>no data</p>
+                    @endif
 
-                        <br>
+                    <br>
 
-                    </div>
                 </div>
-                <div class="col-3">
-                    @include('home_page.section.saran_masukan')
-                </div>
-
+            </div>
+            <div class="col-3">
+                @include('home_page.section.saran_masukan')
             </div>
 
         </div>
 
+    </div>
 
-    @endsection
+
+@endsection
 
 
-    @section('script')
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-        <script>
-            @if (session()->has('success'))
-                swal("Success!", "Terima kasih atas saran dan masukannya", "success");
-                {{ session()->forget('success') }}
-            @endif
-        </script>
-    @endsection
+@section('script')
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script>
+        @if (session()->has('success'))
+            swal("Success!", "Terima kasih atas saran dan masukannya", "success");
+            {{ session()->forget('success') }}
+        @endif
+    </script>
+@endsection
